50f7c80d06e04772933af19e62f69d6845760c1c
Drozmog <drozmog@gmail.com>
2026-06-17 05:15:32 +0500

    Add shared phase script

---
+++ battlefield/PhaseShared.gd
@@ -0,0 +1,7 @@
+class_name PhaseShared
+extends "res://battlefield/BattlefieldManagerPhase35CombatReadability.gd"
+
+const BOARD_ACTION_CHECK: int = 3
+const BLUFF_REVEAL_DELAY: float = 0.30
+
+var enemy_fortified_lanes: Dictionary = {}
