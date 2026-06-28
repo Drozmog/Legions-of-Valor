96f3cb390e2a9b224712b1e7bffa3e6875c4cd04
Drozmog <drozmog@gmail.com>
2026-06-28 06:33:48 +0500

    Add battlefield UI followup

---
+++ battlefield/BattlefieldManagerUiFollowup.gd
@@ -0,0 +1,5 @@
+class_name BattlefieldManagerUiFollowup
+extends "res://battlefield/BattlefieldManagerIconPatch.gd"
+
+func show_mobility_prompt(text: String) -> void:
+	phase_title_overlay.text = text
